1b6b3542e3fee7e20e262f86f9b620bb521cbf54
mafaldamaia <69861212+mafaldamaia@users.noreply.github.com>
2026-09-03 11:32:59 +0100

    Update relationId and languages in config.php

--- config.php
+++ config.php
@@ -4,10 +4,10 @@ declare(strict_types=1);
 
 return [
     // Relation ID of the OpenstreetMap City
-    'relationId' => 0,
+    'relationId' => 3372453,
 
     // Languages in which you want to extract Wiki informations
-    'languages' => ['en'],
+    'languages' => ['en', 'pt'],
 
     // Exclude datas from beeing extracted. Usefull to exclude streets at the edge of the city for example
     'exclude' => [
